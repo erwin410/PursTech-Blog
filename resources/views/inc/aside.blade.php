3c9633c73ab16ebdf70cfb70dccbae22b3d4f28a
lvsprod <erwinpateux@gmail.com>
2022-09-19 14:27:17 +0400

    Début du backoffice

--- resources/views/inc/aside.blade.php
+++ resources/views/inc/aside.blade.php
@@ -3,7 +3,7 @@
     <div class="w-full bg-white shadow flex flex-col my-4 p-6">
         <p class="text-xl font-semibold pb-5">A Propos de nous</p>
         <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
-        <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+        <a href="#" class="w-full bg-[#382A43] text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
             En savoir plus
         </a>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,9 +23,8 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('posts', PostController::class)
          ->except('index');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
